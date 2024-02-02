add97f41b6a987a5575c187697dce0d56828821b
Mugunthan <mugunthan701@gmail.com>
2024-02-02 16:11:36 +0530

    converted to json

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var app = express()
 
 app.get('/ping', (req, res) => {
-    res.send('pong');
+    res.json({message:'pong'});
   });
   app.listen(3002,()=>{
     console.log("this is running on port 3002")
