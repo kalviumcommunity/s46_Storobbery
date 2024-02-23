0291216f598e0d55bb8df159b14dd4b09e329828
Mugunthan <mugunthan701@gmail.com>
2024-02-23 10:47:28 +0530

    added functions

---
+++ Back-End/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgQCtqAMM2bRtUhfZaUv8cSKWOG_oiU3k",
+  authDomain: "storoberry-dd099.firebaseapp.com",
+  projectId: "storoberry-dd099",
+  storageBucket: "storoberry-dd099.appspot.com",
+  messagingSenderId: "578313544514",
+  appId: "1:578313544514:web:eea7aa8d8fc8c3a2c9dedb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
